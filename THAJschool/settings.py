48b00bc34a2ff7cd67ca6baf5b2b983dcbb6f3bc
abdul <abdulhasib2030@gmail.com>
2023-09-20 15:56:40 +0600

    extra text added

--- THAJschool/settings.py
+++ THAJschool/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS =  [
     BASE_DIR/'static',
 ]
